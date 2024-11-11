a7038c6e6d72b1acd32aed6c2be5d346d5c963d2
OhSigma <92277424+NormalIsIncompetent@users.noreply.github.com>
2024-11-11 14:57:03 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let sigmaHeader = document.getElementById("sigmaHeader");
 let clicks = 0;
 
-function clickFunction(clicks){
+function clickFunction(){
   console.log("clicker sigma game");
   clicks++;
   sigmaHeader.innerHTML = "SIGMA CLICKS:"+clicks
